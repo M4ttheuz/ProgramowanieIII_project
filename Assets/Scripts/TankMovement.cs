87d1d55efdad59466d8ddbf2202acae3bc112814
Mateusz Białek <122973031+M4ttheuz@users.noreply.github.com>
2024-11-08 16:11:41 +0100

    Update TankMovement.cs

--- Assets/Scripts/TankMovement.cs
+++ Assets/Scripts/TankMovement.cs
@@ -2,19 +2,17 @@ using UnityEngine;
 
 public class TankMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;        // Prêdkoœæ ruchu czo³gu do przodu
-    public float rotationSpeed = 100f;  // Prêdkoœæ obrotu czo³gu
+    public float moveSpeed = 5f;      
+    public float rotationSpeed = 100f; 
 
     void Update()
     {
-        // Pobieranie wejœcia (Input) z klawiatury
-        float move = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; // Ruch przód-ty³
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime; // Obrót
+        
+        float move = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; 
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
 
-        // Ruch czo³gu do przodu i do ty³u
         transform.Translate(0, 0, move);
 
-        // Obrót czo³gu wokó³ osi Y
         transform.Rotate(0, rotation, 0);
     }
 }
